e8e0d56c32e578bf31cf6ff6614098792e733814
Ralph Barton <ralph_barton_@hotmail.com>
2017-06-15 18:50:10 +0100

    Features implemented: Toggle Numbering, and Toggle Indent  - some neatening up of the code (helper functions reside in their own objects, for structure). Better naming conventions...  - States of 'useIndent' and 'useNumbering' are read out of spreadsheet, and control the operation of 'reformatHierarchy()'

--- code.js
+++ code.js
@@ -1,76 +1,119 @@
-var numberingCounters = [0, 0, 0, 0, 0];
-var prevDepth = 0;
-function nextAutoNumber(depth){
 
-    if(depth < prevDepth){
 
-	//reset them all to a value of 0
-	for(var i = prevDepth; i > depth; i--){
-	    numberingCounters[i] = 0;
+// I'm making this statement global
+var sheet = SpreadsheetApp.getActiveSheet();
+
+
+// Helper functions...
+var autoNumbering = {
+
+    numberingCounters: [0, 0, 0, 0, 0],
+    prevDepth: 0,
+
+    genNext: function(depth){
+
+	if(depth < this.prevDepth){
+
+	    //reset them all to a value of 0
+	    for(var i = this.prevDepth; i > depth; i--){
+		this.numberingCounters[i] = 0;
+	    }
+	    
 	}
-	
+
+	this.numberingCounters[depth]++;
+	this.prevDepth = depth;
+
+	var retString = "";
+	for(var i = 0; i <= depth; i++){
+	    retString += this.numberingCounters[i] + ".";
+	}
+	return retString + " ";
     }
-
-    numberingCounters[depth]++;
-    prevDepth = depth;
-
-    var retString = "";
-    for(var i = 0; i <= depth; i++){
-	retString += numberingCounters[i] + ".";
-    }
-    return retString + " ";
-}
+};
 
 
 
-var fontSizes = [18, 14, 12, 10, 8];
 
-function changeColumns() {
-    var sheet = SpreadsheetApp.getActiveSheet();
+
+
+
+
+var fontSizes = [18, 14, 11, 10, 8];
+var rowHeights = [65, 21, 21, 18, 16];
+
+//how many colums are to the left of the top level of hierarchy?
+var qtyLeftColumns = 1;
+
+function reformatHierarchy() {
     var data = sheet.getDataRange().getValues();
-
-    // Let's assume a max depth of 5
-    // i.e. 1.1.1.2.1
 
     //REMEMBER - items in Data array are 0-indexed.
     // row/column ranges are 1-indexed.
     
+    // addressing cells using (R, C)
+    var useIndent =    sheet.getRange(2, 16).getValue();
+    var useNumbering = sheet.getRange(3, 16).getValue();    
+
+    
+    
     for (var i = 0; i < data.length; i++) {
 
 	var rowIndex = i+1;
-	var specifiedCol = data[i][0];
+
+	//hierchy-depth of this item (row). Top-level is value 1.
+	var item_HDepth = data[i][0];
+
+	// Assumed a max depth of 5 - i.e. 1.1.1.2.1
 	var itemTitle = data[i][1] || data[i][2] || data[i][3] || data[i][4] || data[i][5];
 
 	
-	if(specifiedCol){
+	if(item_HDepth){
 
 
-	    // Extract text from any preceeding numbers...
+	    // Generate new Title Text...
+	    // 1. Extract text from any preceeding numbers...
 	    var firstLetter = itemTitle.match(/[a-zA-Z]/);
 	    var firstLetIndex = itemTitle.indexOf(firstLetter);
 	    var cleanTitle = itemTitle.substring(firstLetIndex);
 
-	    var numberedTitle = nextAutoNumber(specifiedCol-1) + cleanTitle;
+	    // 2. whether to 'use numbering' is an option...
+	    var newTitleText = (useNumbering ? autoNumbering.genNext(item_HDepth-1) : "") + cleanTitle;
 	    
-	    // heartbeat
-	    Logger.log(rowIndex + ": r-retrieve" + specifiedCol);
-
 	    // clear the original text
 	    // getRange(row, column, numRows, numColumns)
 	    var row5cols = sheet.getRange(rowIndex, 2, 1, 5);
 	    row5cols.clearContent();
 
 	    //set font size
-	    var myFontSize = fontSizes[specifiedCol-1];
+	    var myFontSize = fontSizes[item_HDepth-1];
 	    row5cols.setFontSize(myFontSize);
 
 	    //set row height, if its a level 1 heading.
-	    var myHeight = specifiedCol === 1 ? 65 : 21;
+	    var myHeight = rowHeights[item_HDepth-1];
 	    sheet.setRowHeight(rowIndex, myHeight);
 	    
 	    // replace it into desired column
-	    var targetCell = sheet.getRange(rowIndex, specifiedCol+1);
-	    targetCell.setValue(numberedTitle);
+	    var columnIndex = qtyLeftColumns + (useIndent ? item_HDepth : 1);
+	    var targetCell = sheet.getRange(rowIndex, columnIndex);
+	    targetCell.setValue(newTitleText);
 	}
     }
 }
+
+
+
+// Composite functions
+
+function toggleIndent(){
+    var newValue = 1 - sheet.getRange(2, 16).getValue();
+    sheet.getRange(2, 16).setValue(newValue);
+    reformatHierarchy();
+};
+
+
+function toggleNumbering(){
+    var newValue = 1 - sheet.getRange(3, 16).getValue();
+    sheet.getRange(3, 16).setValue(newValue);
+    reformatHierarchy();
+};
